bc0588720fa7cf15c59b887cfacd2d5888bb51bf
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-12-18 21:58:17 +0800

    Update default.html and update_last_modified.sh

--- scripts/update_last_modified.sh
+++ scripts/update_last_modified.sh
@@ -9,23 +9,27 @@ for file in $pages; do
   # Get the last Git commit timestamp for the current file
   last_modified=$(git log -1 --format="%ad" --date=iso "$file")
 
-  # Check if the file has a YAML front matter
-  if grep -q "^---" "$file"; then
-    # If the file has a front matter, update or add the last_modified_at field
-    if grep -q "^last_modified_at:" "$file"; then
-      # Update the existing last_modified_at field
-      sed -i.bak -E "s/^(last_modified_at:).*/\1 \"$last_modified\"/" "$file"
+  if ! git diff --quiet HEAD -- "$file"; then
+    # Check if the file has a YAML front matter
+    if grep -q "^---" "$file"; then
+      # If the file has a front matter, update or add the last_modified_at field
+      if grep -q "^last_modified_at:" "$file"; then
+        # Update the existing last_modified_at field
+        sed -i.bak -E "s/^(last_modified_at:).*/\1 \"$last_modified\"/" "$file"
+      else
+        # Add the last_modified_at field to the end of the front matter
+        awk -v lm="$last_modified" '/^---$/{print; if (c++ == 0) print "last_modified_at: \"" lm "\""; next} 1' "$file" > temp_file && mv temp_file "$file"
+      fi
     else
-      # Add the last_modified_at field to the end of the front matter
-      awk -v lm="$last_modified" '/^---$/{print; if (c++ == 0) print "last_modified_at: \"" lm "\""; next} 1' "$file" > temp_file && mv temp_file "$file"
+      # If the file does not have a front matter, add one with the last_modified_at field
+      echo -e "---\nlast_modified_at: \"$last_modified\"\n---\n$(cat "$file")" > "$file"
     fi
-  else
-    # If the file does not have a front matter, add one with the last_modified_at field
-    echo -e "---\nlast_modified_at: \"$last_modified\"\n---\n$(cat "$file")" > "$file"
-  fi
 
-  # Log the updated file and its timestamp
-  echo "Updated last_modified_at for $file to $last_modified"
+    # Log the updated file and its timestamp
+    echo "Updated last_modified_at for $file to $last_modified"
+  else
+    echo "No changes detected for $file; skipping update."
+  fi
 done
 
 # Remove backup files created during sed operation
